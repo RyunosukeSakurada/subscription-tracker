f253e9c9ecf908950cbe67493bf193961186f4b9
RyunosukeSakurada <longzhizhu513@gmail.com>
2023-05-12 23:37:54 -0700

    added ui

--- subscription-tracker/src/components/SubscriptionList.jsx
+++ subscription-tracker/src/components/SubscriptionList.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import SubscriptionItem from './SubscriptionItem';
+
 
 function SubscriptionList() {
+
   return (
-    <div>
-      subscription list
+    <div className='my-20'>
+      <h2 className='text-xl lg:text-2xl'>Subscriptions</h2>
+      <ul className='max-w-[700px] mx-auto'>
+        <SubscriptionItem />
+      </ul>
     </div>
   )
 }
