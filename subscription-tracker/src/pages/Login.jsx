7ced1ead5b9a3a3dfac6817439c425ec1cac36f6
RyunosukeSakurada <longzhizhu513@gmail.com>
2023-05-14 19:39:34 -0700

    done

--- subscription-tracker/src/pages/Login.jsx
+++ subscription-tracker/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import firebase from "firebase/compat/app";
 import { auth } from "../firebase.js";
 import Button from "@mui/material/Button";
+import Img from "../assets/login.jpg"
 
 
 function Login() {
@@ -11,14 +12,24 @@ function Login() {
   }
 
   return (
-    <div>
-      <Button 
-        onClick={signInWithGoogle}
-        variant="outlined"
-        color="info"
-      >
-          Login
-      </Button>
+    <div className="max-w-[900px] mx-auto px-8 py-20 h-screen">
+      <p className="text-3xl" >Wellcome to our</p>
+      <span className="text-5xl text-cyan-500 font-bold ">Subscription Tracker</span>
+      <img 
+        src={Img}
+        alt="" 
+        className="w-full h-[300px] object-cover mt-20 rounded mb-20"
+      />
+      <div>
+        <Button 
+          onClick={signInWithGoogle}
+          variant="outlined"
+          color="info"
+          sx={{ width: 250, padding: 1.5}}
+        >
+            Login
+        </Button>
+      </div>
     </div>
   )
 }
